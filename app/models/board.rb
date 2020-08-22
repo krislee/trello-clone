1ead64cd832ba13d539193018940ad812fedb2ef
Kristy Lee <leekristy01@gmail.com>
2020-08-22 12:40:16 -0400

    dropped the model/migration files and redid them in order

--- app/models/board.rb
+++ app/models/board.rb
@@ -3,5 +3,5 @@ class Board < ApplicationRecord
   has_many :items, through: :lists
   belongs_to :user
 
-  validates_presence_of :board_title # needs to have a title for the board to actually form the board
+  validates_presence_of :board_title
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :boards, dependent: :destroy
-  has_many :lists, through: boards
+  has_many :lists, through: :boards
   has_many :items, through: :lists
   validates_presence_of :username, :password_digest
 end
