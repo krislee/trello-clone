285a86baf305082caf2c2a7cb6ba1c8ccdcd396c
Kristy Lee <leekristy01@gmail.com>
2020-08-23 14:35:06 -0400

    deployment issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,3 @@
 Rails.application.routes.draw do
-  resource :users, only: [:create]
-  post "/login", to: "users#login"
-  get "/auto_login", to: "users#auto_login"
-
-  resources :boards do
-    resources :lists do
-      resources :items do
-      end
-    end
-  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
-
