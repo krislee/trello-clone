b2712a84fb413621af8ab2bf857b79fdbc51ef14
Kristy Lee <leekristy01@gmail.com>
2020-08-23 10:50:46 -0400

    added secret key in .env

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
   before_action :authorized
 
   def encode_token(payload)
-    JWT.encode(payload, 'yourSecret')
+    JWT.encode(payload, ENV['SECRET'])
   end
 
   def auth_header
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::API
       token = auth_header.split(' ')[1]
       # header: { 'Authorization': 'Bearer <token>' }
       begin
-        JWT.decode(token, 'yourSecret', true, algorithm: 'HS256')
+        JWT.decode(token, ENV['SECRET'], true, algorithm: 'HS256')
       rescue JWT::DecodeError
         nil
       end
